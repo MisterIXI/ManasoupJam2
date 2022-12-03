c16d8767dc539b3852f1ddf76ae29a100bb96339
Yannik <yannik.braendle@t-online.de>
2022-12-03 21:55:07 +0100

    chargeKohl initial

--- Oneiromancy/Assets/_Scripts/Enemies/ChargeEnemy.cs
+++ Oneiromancy/Assets/_Scripts/Enemies/ChargeEnemy.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ChargeEnemy : Enemy
 {
     private bool IsCharging;
-    private float ChargeStartTime;
     protected override void Init()
     {
         CurrentHealth = Random.Range(_playerSettings.ChargeEnemyHealthRange.x, _playerSettings.ChargeEnemyHealthRange.y + 1);
@@ -14,28 +13,34 @@ public class ChargeEnemy : Enemy
 
     public override void EnemyTick()
     {
-        // float Distance = Vector3.Distance(transform.position, _playerTransform.position);
-        // if(Distance < _playerSettings.ChargeEnemyChargeRange)
-        // {
-        //     if(!IsCharging)
-        //     {
-        //         IsCharging = true;
-        //         ChargeStartTime = Time.time;
-        //     }
-        //     if(Time.time - ChargeStartTime > _playerSettings.ChargeEnemyChargeTime)
-        //     {
-        //         Charge();
-        //     }
-        // }
-        // else
-        // {
-        //     IsCharging = false;
-        //     Vector3 Target = _playerTransform.position;
-        //     Target.y = transform.position.y;
-        //     transform.LookAt(Target);
-        //     Vector3 MoveTarget = transform.position + transform.forward * _playerSettings.EnemySpeed * Time.fixedDeltaTime;
-        //     MoveTarget.y = transform.position.y;
-        //     _rb.MovePosition(MoveTarget);
-        // }
+        float Distance = Vector3.Distance(transform.position, _playerTransform.position);
+        if (!IsCharging)
+        {
+            Vector3 Target = _playerTransform.position;
+            Target.y = transform.position.y;
+            transform.LookAt(Target);
+            if (Distance < _playerSettings.CE_Range)
+            {
+                IsCharging = true;
+                _rb.velocity = Vector3.zero;
+                _rb.angularVelocity = Vector3.zero;
+                StartCoroutine(Charge());
+            }
+            else
+            {
+                Vector3 MoveTarget = transform.position + transform.forward * _playerSettings.EnemySpeed * Time.fixedDeltaTime;
+                MoveTarget.y = transform.position.y;
+                _rb.MovePosition(MoveTarget);
+            }
+        }
+    }
+
+    IEnumerator Charge()
+    {
+        yield return new WaitForSeconds(_playerSettings.CE_ChargeTime);
+        IsCharging = false;
+        // impluse forward
+        Debug.Log("Charge");
+        _rb.AddForce(transform.forward * _playerSettings.CE_ImpluseMultiplier * 20f, ForceMode.Impulse);
     }
 }
